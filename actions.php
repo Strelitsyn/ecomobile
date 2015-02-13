700a6c5a0fff15f4bfa6db04e0d4ed31b648042f
strelitsyn <skifskifskif@bk.ru>
2015-02-13 18:32:02 +0300

    init

---
+++ actions.php
@@ -0,0 +1,26 @@
+﻿<?
+/*if ($_POST["send"]) {
+	echo($_POST["num"]);
+}*/
+
+if(is_uploaded_file($_FILES["photo"]["tmp_name"])){
+	$newFileName = uniqid() . ".jpg";
+	if (move_uploaded_file($_FILES["photo"]["tmp_name"], "upload/" . $newFileName) == false){
+		$error = $_FILES["photo"]["name"].": Ошибка при загрузке файла";
+	}
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>title</title>
+		<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+	</head>
+	
+	<body>
+		<img src="upload/<?=$newFileName?>" style="width: 200px; height: 200px">
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<title>title</title>
+		<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+	</head>
+	
+	<body>
+		<form action="actions.php" enctype="multipart/form-data" method="post">
+			<input type="text" name="num"></input><br/><br/>
+			<input type="submit" name="send" value="Send"></input>
+		</form>
+	</body>
+</html>
